57020f6ab119adc773e62cfc6c0d0e13465b62fd
Fletcher Sarip <fletchersarip@gmail.com>
2020-12-01 17:23:18 +0000

    fix: Add missing word_dict parameter in the convert_and_pad function call

--- serve/predict.py
+++ serve/predict.py
@@ -69,7 +69,7 @@ def predict_fn(input_data, model):
     #       You should produce two variables:
     #         data_X   - A sequence of length 500 which represents the converted review
     #         data_len - The length of the review
-    data_X, data_len = convert_and_pad(review_to_words(input_data))
+    data_X, data_len = convert_and_pad(model.word_dict, review_to_words(input_data))
 
     # Using data_X and data_len we construct an appropriate input tensor. Remember
     # that our model expects input data of the form 'len, review[500]'.
